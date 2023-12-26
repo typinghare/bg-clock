8982c9a4faa12614df2ea20188028ac276a5f77d
James Chan <jameschan312.cn@gmail.com>
2023-12-25 21:20:03 -0500

    fix: added build base "/bgc/"

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 // noinspection JSUnusedGlobalSymbols
 export default defineConfig({
+    base: '/bgc/',
     plugins: [react()],
     server: {
         open: true,
